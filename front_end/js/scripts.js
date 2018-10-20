0c3382f2c32f0b5f69857c499349f81233fd6932
Evan Kirby McGregor <Evan@localhost.localdomain>
2018-10-20 12:36:43 -0400

    testing video

--- front_end/js/scripts.js
+++ front_end/js/scripts.js
@@ -3,6 +3,13 @@
 * JavaScript functions for the Space-Apps challenge 2018 - Rocket Launch Tracker
 */
 
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
