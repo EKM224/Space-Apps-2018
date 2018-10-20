d308642561c4b9084de9f6abcb82c2c2beed6f8e
Alexandre Falardeau <alexandre.h.falardeau@gmail.com>
2018-10-20 19:58:47 -0400

    merge resolved

---
+++ front_end/views.js
@@ -0,0 +1,17 @@
+var ListView = {
+    update: function() { 
+        Model.launches.forEach((launch))
+    }
+}
+
+var VideoView = {
+    update: function() {
+
+    }
+}
+
+var IndividualLaunchView = {
+    update: function() {
+
+    }
+}
