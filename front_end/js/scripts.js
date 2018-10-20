617c32fd1d8b0b72d1db53cbfd55c843970dd2f9
Evan Kirby McGregor <Evan@localhost.localdomain>
2018-10-20 14:14:44 -0400

    merge video and navbar

--- front_end/js/scripts.js
+++ front_end/js/scripts.js
@@ -7,8 +7,6 @@ var player;
 
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('video-placeholder', {
-        width: "100%",
-        height: "100%",
         frameborder: "0",
         videoId: 'sB_nEtZxPog',
         playerVars: {
@@ -28,20 +26,6 @@ function onYouTubeIframeAPIReady() {
 }
 
 function initialize(event){
-
-  /* // Update the controls on load
-  updateTimerDisplay();
-  updateProgressBar();
-
-  // Clear any old interval.
-  clearInterval(time_update_interval);
-
-  // Start interval to update elapsed time display and
-  // the elapsed part of the progress bar every second.
-  time_update_interval = setInterval(function () {
-      updateTimerDisplay();
-      updateProgressBar();
-  }, 1000) */
 
   event.target.setVolume(50);
   event.target.mute();
