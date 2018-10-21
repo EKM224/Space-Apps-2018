609ca75b44f136807587b2b9b3d46fb3dcd75799
Nathanael Beaudoin-Dion <nathanael.beaudoin-dion@polymtl.ca>
2018-10-21 11:29:08 -0400

    update, works for spacex

--- back_end/scrapper/scrapping_test.py
+++ back_end/scrapper/scrapping_test.py
@@ -1,22 +1,26 @@
 import requests
 import bs4
 
-url1 = 'https://oronospolytechnique.com'
-url2 = 'https://www.nasa.gov/launchschedule/'
-url3 = 'https://www.spacex.com/news'
+url1 = 'https://en.wikipedia.org/wiki/Falcon_9'
+url2 = ''
 
 
-def get_video_page_urls():
+def get_success_spacex():
     response = requests.get(url1)
     soup = bs4.BeautifulSoup(response.text, features="html.parser")
     # return [a.attrs.get('href') for a in soup.select('div.title a[href^=/video]')]
-    links = soup.select('div.section-heading h2')
-    #print(response.text)
-    return links
+    links = soup.select('div.hlist li')
+    # print(response.text)
+
+    string = str(links)
+    finding1 = string.find('"List of Falcon 9 and Falcon Heavy launches">')
+    finding_end = string.find('</a>')
+    finding_begin = len('"List of Falcon 9 and Falcon Heavy launches">') + finding1
+    numbers = string[finding_begin:finding_end]
+    print(numbers)
 
 
-print(get_video_page_urls())
-
+get_success_spacex()
 
 # https://blog.miguelgrinberg.com/post/easy-web-scraping-with-python
 
